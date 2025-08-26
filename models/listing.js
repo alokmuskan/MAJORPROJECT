6a6e179d599345028df7edb0b75072a5a575453f
alokraj <alokraj1319@gmail.com>
2025-08-26 22:19:06 +0530

    while adding new details of a myhome image was not loading bcoz of the url i have copied wrong url i should have copied image url but i have copied page url

--- models/listing.js
+++ models/listing.js
@@ -9,10 +9,12 @@ const listingSchema = new Schema ({
     description: String,
     image: {
         type: String,
-        default: "https://unsplash.com/photos/ocean-waves-crashing-on-shore-during-sunset-N5sC8zZUgy4" ,
-        set: (v) => v === ""
-         ? "https://unsplash.com/photos/ocean-waves-crashing-on-shore-during-sunset-N5sC8zZUgy4" 
-         : v,
+        default: 
+            "https://plus.unsplash.com/premium_photo-1689609950112-d66095626efb?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" ,
+        set: (v) => 
+            v === ""
+                ? "https://plus.unsplash.com/premium_photo-1689609950112-d66095626efb?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" 
+                : v,
     },
     price: Number,
     location: String,
