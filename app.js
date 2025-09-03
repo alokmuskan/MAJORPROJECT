494803541f57bfc447a519c2a9f530096168e37d
alokraj <alokraj1319@gmail.com>
2025-09-03 10:41:07 +0530

    joi identifies the error and prints in the terminal

--- app.js
+++ app.js
@@ -60,8 +60,12 @@ app.get("/listings/:id", wrapAsync(async (req, res) => {
 //Create Route
 app.post("/listings", 
     wrapAsync(async (req, res, next) => {
+        let result = listingSchema.validate(req.body);
+        console.log(result);
+        if(result.error) {
+            throw new ExpressError(400, result.error);
+        }
         const newListing = new Listing(req.body.listing);
-        
         await newListing.save();
         res.redirect("/listings");
     })
