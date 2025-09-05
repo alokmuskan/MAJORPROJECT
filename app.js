bd6e5c26349746df5fe4309eddb4350cd6220b44
alokraj <alokraj1319@gmail.com>
2025-09-05 11:45:59 +0530

    set up post route for reviews of each listing and inserts review in db but for now we cannot see them

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const ejsMate = require("ejs-mate");
 const wrapAsync = require("./utils/wrapAsync.js");
 const ExpressError = require("./utils/ExpressError.js"); 
 const { listingSchema } = require("./schema.js");
+const Review = require("./models/review.js");
 
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderLust";
@@ -109,6 +110,21 @@ app.delete("/listings/:id", wrapAsync(async(req, res) => {
 }));
 
 
+//Reviews
+//Post Route
+app.post("/listings/:id/reviews", async(req, res,) => {
+    let listing = await Listing.findById(req.params.id);
+    let newReview = new Review(req.body.review);
+
+    listing.reviews.push(newReview);
+
+    await newReview.save();
+    await listing.save();
+
+    // console.log("new review saved");
+    // res.send("new review saved");
+    res.redirect(`/listings/${listing._id}`);
+});
 
 //it responds to the routes that do not exists and acts like a wild
 app.use((req, res, next) => {
