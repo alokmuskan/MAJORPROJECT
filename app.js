35be770c6fd6afba093d74901c0a09798af0c9c0
alokraj <alokraj1319@gmail.com>
2025-09-05 21:47:11 +0530

    required listing.js in app.js and removed /listing from all the listing routes and the server crashes now as we have used several middlewares and functions that needs to be required in listing.js

--- app.js
+++ app.js
@@ -10,8 +10,11 @@ const ExpressError = require("./utils/ExpressError.js");
 const { listingSchema, reviewSchema } = require("./schema.js");
 const Review = require("./models/review.js");
 
+const listings = require("./routes/listing.js");
+
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderLust";
+
 main()
 .then(() => {
     console.log("connected to DB");
@@ -60,66 +63,7 @@ const validateReview = (req, res, next) => {
     }
 } 
 
-//Index route
-app.get("/listings", wrapAsync(async(req, res) => {
-    const allListings = await Listing.find({});
-    res.render("listings/index.ejs", { allListings });
-}));
-
-
-//New Route
-app.get("/listings/new", (req, res) => {
-    res.render("listings/new.ejs");
-});
-
-
-//Show Route
-app.get("/listings/:id", wrapAsync(async (req, res) => {
-    let { id } = req.params;
-    const listing = await Listing.findById(id).populate("reviews");
-    res.render("listings/show.ejs", {listing});
-}));
-
-
-//Create Route
-app.post("/listings", 
-    validateListing,
-    wrapAsync(async (req, res, next) => {
-        const newListing = new Listing(req.body.listing);
-        await newListing.save();
-        res.redirect("/listings");
-    })
-);
-
-
-//Edit Route
-app.get("/listings/:id/edit", wrapAsync(async(req, res) => {
-    let { id } = req.params;
-    const listing = await Listing.findById(id);
-    res.render("listings/edit.ejs", { listing });
-}));
-
-
-//Update Route 
-app.put(
-    "/listings/:id", 
-    validateListing,
-    wrapAsync(async(req, res) => {
-        let { id } = req.params;
-        await Listing.findByIdAndUpdate(id, {...req.body.listing});
-        res.redirect(`/listings/${id}`);
-    })
-);
-
-
-//Delete Route
-app.delete("/listings/:id", wrapAsync(async(req, res) => {
-    let { id } = req.params;
-    let deletedListing = await Listing.findByIdAndDelete(id);
-    console.log(deletedListing);
-    res.redirect("/listings");
-}));
-
+app.use("/listings", listings);
 
 //Reviews
 
--- routes/listings.js
+++ routes/listings.js
@@ -1,3 +1,65 @@
 const express = require("express");
 const router = express.Router();
 
+//Index route
+router.get("/", wrapAsync(async(req, res) => {
+    const allListings = await Listing.find({});
+    res.render("listings/index.ejs", { allListings });
+}));
+
+
+//New Route
+router.get("/new", (req, res) => {
+    res.render("listings/new.ejs");
+});
+
+
+//Show Route
+router.get("/:id", wrapAsync(async (req, res) => {
+    let { id } = req.params;
+    const listing = await Listing.findById(id).populate("reviews");
+    res.render("listings/show.ejs", {listing});
+}));
+
+
+//Create Route
+router.post("/", 
+    validateListing,
+    wrapAsync(async (req, res, next) => {
+        const newListing = new Listing(req.body.listing);
+        await newListing.save();
+        res.redirect("/listings");
+    })
+);
+
+
+//Edit Route
+router.get("/:id/edit", wrapAsync(async(req, res) => {
+    let { id } = req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/edit.ejs", { listing });
+}));
+
+
+//Update Route 
+router.put(
+    "/:id", 
+    validateListing,
+    wrapAsync(async(req, res) => {
+        let { id } = req.params;
+        await Listing.findByIdAndUpdate(id, {...req.body.listing});
+        res.redirect(`/listings/${id}`);
+    })
+);
+
+
+//Delete Route
+router.delete("/:id", wrapAsync(async(req, res) => {
+    let { id } = req.params;
+    let deletedListing = await Listing.findByIdAndDelete(id);
+    console.log(deletedListing);
+    res.redirect("/listings");
+}));
+
+
+module.exports = router;
