6bbadf90e62bd1154ff45353480694e9bea05172
alokraj <alokraj1319@gmail.com>
2025-09-05 12:35:47 +0530

    created reviewSchema in schema.js for server side validation of reviews and required it in app.js then written separate schema validation function (middleware) and passed it thru reviews route

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const wrapAsync = require("./utils/wrapAsync.js");
 const ExpressError = require("./utils/ExpressError.js"); 
-const { listingSchema } = require("./schema.js");
+const { listingSchema, reviewSchema } = require("./schema.js");
 const Review = require("./models/review.js");
 
 
@@ -40,6 +40,17 @@ app.get("/", (req, res) => {
 const validateListing = (req, res, next) => {
     let { error } = listingSchema.validate(req.body);
     // console.log(result);
+    if(error) {
+        let errMsg = error.details.map((el) => el.message).join(",");
+        throw new ExpressError(400, errMsg);
+    }
+    else {
+        next();
+    }
+} 
+
+const validateReview = (req, res, next) => {
+    let { error } = reviewSchema.validate(req.body);
     if(error) {
         let errMsg = error.details.map((el) => el.message).join(",");
         throw new ExpressError(400, errMsg);
@@ -112,7 +123,9 @@ app.delete("/listings/:id", wrapAsync(async(req, res) => {
 
 //Reviews
 //Post Route
-app.post("/listings/:id/reviews", async(req, res,) => {
+app.post("/listings/:id/reviews", 
+    validateReview , 
+    wrapAsync(async(req, res,) => {
     let listing = await Listing.findById(req.params.id);
     let newReview = new Review(req.body.review);
 
@@ -124,7 +137,7 @@ app.post("/listings/:id/reviews", async(req, res,) => {
     // console.log("new review saved");
     // res.send("new review saved");
     res.redirect(`/listings/${listing._id}`);
-});
+}));
 
 //it responds to the routes that do not exists and acts like a wild
 app.use((req, res, next) => {
