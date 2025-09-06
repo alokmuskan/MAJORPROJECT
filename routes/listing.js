294d80156025ce8c2042b7dd7f73e3b02ad97181
alokraj <alokraj1319@gmail.com>
2025-09-07 00:07:03 +0530

    created error partial alert for failure whenever we are searching a listing and it does not exists

--- routes/listing.js
+++ routes/listing.js
@@ -35,6 +35,10 @@ router.get("/new", (req, res) => {
 router.get("/:id", wrapAsync(async (req, res) => {
     let { id } = req.params;
     const listing = await Listing.findById(id).populate("reviews");
+    if(!listing) {
+        req.flash("error", "Listing you requested for does not exist!");
+        res.redirect("/listings");
+    }
     res.render("listings/show.ejs", {listing});
 }));
 
