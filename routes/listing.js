03ea30178e68c5871d0118ea8a1ab5f3839cea50
alokraj <alokraj1319@gmail.com>
2025-09-07 00:29:10 +0530

    added error partial alert for edit route that while editing a listing if we copy its address and delete it later and then try to search for the same address then we should get a error/failure alert

--- routes/listing.js
+++ routes/listing.js
@@ -60,6 +60,10 @@ router.post("/",
 router.get("/:id/edit", wrapAsync(async(req, res) => {
     let { id } = req.params;
     const listing = await Listing.findById(id);
+     if(!listing) {
+        req.flash("error", "Listing you requested for does not exist!");
+        return res.redirect("/listings");
+    }
     res.render("listings/edit.ejs", { listing });
 }));
 
