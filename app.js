e6fe2ee3fc622d7659d9976142f90d29fe224844
alokraj <alokraj1319@gmail.com>
2025-09-08 01:47:48 +0530

    commented out the fakeuser that we created earlier to login as its of no use now

--- app.js
+++ app.js
@@ -75,15 +75,15 @@ app.use((req, res, next) => {
 });
 
 
-app.get("/demouser", async(req, res) => {
-    let fakeUser = new User({
-        email: "student@gmail.com",
-        username: "delta-student"
-    });
+// app.get("/demouser", async(req, res) => {
+//     let fakeUser = new User({
+//         email: "student@gmail.com",
+//         username: "delta-student"
+//     });
 
-    let registeredUser = await User.register(fakeUser, "helloworld"); 
-    res.send(registeredUser);
-})
+//     let registeredUser = await User.register(fakeUser, "helloworld"); 
+//     res.send(registeredUser);
+// })
 
 
 app.use("/listings", listingRouter);
