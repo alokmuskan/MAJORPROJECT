8721144289701ba18570b73c7ac9e2f349899de0
alokraj <alokraj1319@gmail.com>
2025-09-08 02:37:06 +0530

    created a async post router with an authenticate() function which is route middleware that authenticate the requests means matches whether provided username and password is correct or not and also added failureRedirect and flash msg to it then if credentials are corrects it redirects to /listings

--- routes/user.js
+++ routes/user.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/user.js");
 const wrapAsync = require("../utils/wrapAsync");
+const passport = require("passport");
 
 router.get("/signup", (req, res) => {
     res.render("users/signup.ejs");
@@ -29,5 +30,17 @@ router.get("/login", (req, res) => {
     res.render("users/login.ejs");
 });
 
+
+router.post(
+    "/login", 
+    passport.authenticate("local", {
+    failureRedirect: "/login" , 
+    failureFlash: true
+    }), 
+    async(req, res) => {
+        req.flash("success", "Welcome back to WanderLust!");
+        res.redirect("/listings");
+    }
+)
 
 module.exports = router;
