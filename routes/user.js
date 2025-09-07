22af5b02168ca17234b1a22d12b455423884aab9
alokraj <alokraj1319@gmail.com>
2025-09-08 01:42:10 +0530

    before creating a new user we required our user model on user.js

--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const User = require("../models/user.js");
 
 router.get("/signup", (req, res) => {
     res.render("users/signup.ejs");
@@ -8,6 +9,7 @@ router.get("/signup", (req, res) => {
 
 router.post("/signup", async(req, res) => {
      let { username, email, password } = req.body; 
+    const newUser =  new User({email, username});
 });
 
 module.exports = router;
