53cb3267de8a07f2248806a2296ac2df5ebc2db9
alokraj <alokraj1319@gmail.com>
2025-09-07 23:42:58 +0530

    session shoule be implemented before only because passport uses sessions

--- app.js
+++ app.js
@@ -59,6 +59,8 @@ app.use(session(sessionOptions));
 app.use(flash());
 
 
+
+
 app.use((req, res, next) => {
     res.locals.success = req.flash("success");
     res.locals.error = req.flash("error");
