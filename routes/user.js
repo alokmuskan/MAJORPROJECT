2a37684404e237cabab06f9fbc9e03ab7850ea34
alokraj <alokraj1319@gmail.com>
2025-09-09 23:30:58 +0530

    used req.login method of passport to automatically login after signup

--- routes/user.js
+++ routes/user.js
@@ -9,15 +9,22 @@ router.get("/signup", (req, res) => {
 });
 
 
-router.post("/signup", wrapAsync(async(req, res) => {
+router.post(
+    "/signup", 
+    wrapAsync(async(req, res) => {
     try {
         let { username, email, password } = req.body; 
         const newUser =  new User({email, username});
 
         const registeredUser = await User.register(newUser, password); 
         console.log(registeredUser);
-        req.flash("success", "Welcome to WanderLust");
-        res.redirect("/listings");
+        req.login(registeredUser, (err) => {
+            if(err) {
+                return next(err);
+            }
+            req.flash("success", "Welcome to WanderLust");
+            res.redirect("/listings");
+        });
     } 
     catch(e) {
         req.flash("error", e.message);
