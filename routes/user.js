2f84cb756316db4d68cdc0b76c84a4e06a0689a7
alokraj <alokraj1319@gmail.com>
2025-09-09 22:39:07 +0530

    created a route /logout to signout from the page

--- routes/user.js
+++ routes/user.js
@@ -43,4 +43,16 @@ router.post(
     }
 );
 
+
+router.get("/logout", (req, res, next) => {
+    req.logout((err) => {
+        if(err) {
+            return next(err);
+        }
+        req.flash("success", "you are logged out!");
+        res.redirect("/listings");
+    });
+});
+
+
 module.exports = router;
