c548c26f1061ebcd545d4de004e541e917cf177c
alokraj <alokraj1319@gmail.com>
2025-09-10 22:50:59 +0530

    we have used chainging with populate method to extract the details of owner/user

--- routes/listing.js
+++ routes/listing.js
@@ -35,7 +35,9 @@ router.get("/new", isLoggedIn, (req, res) => {
 //Show Route
 router.get("/:id", wrapAsync(async (req, res) => {
     let { id } = req.params;
-    const listing = await Listing.findById(id).populate("reviews");
+    const listing = await Listing.findById(id)
+    .populate("reviews")
+    .populate("owner");
     if(!listing) {
         req.flash("error", "Listing you requested for does not exist!");
         return res.redirect("/listings");
