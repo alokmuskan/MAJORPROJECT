e2158785e0ef606978f1e71a381bceb3bb5ebd25
alokraj <alokraj1319@gmail.com>
2025-09-13 00:49:24 +0530

    included validateReview in middleware.js and required reviewSchema in middleware.js

--- middleware.js
+++ middleware.js
@@ -1,6 +1,6 @@
 const Listing = require("./models/listing");
 const ExpressError = require("./utils/ExpressError.js"); 
-const { listingSchema } = require("./schema.js");
+const { listingSchema, reviewSchema } = require("./schema.js");
 
 module.exports.isLoggedIn = (req, res, next) => {
     if(!req.isAuthenticated()) {
@@ -34,6 +34,17 @@ module.exports.isOwner = async(req, res, next) => {
 module.exports.validateListing = (req, res, next) => {
     let { error } = listingSchema.validate(req.body);
     // console.log(result);
+    if(error) {
+        let errMsg = error.details.map((el) => el.message).join(",");
+        throw new ExpressError(400, errMsg);
+    }
+    else {
+        next();
+    }
+};
+
+module.exports.validateReview = (req, res, next) => {
+    let { error } = reviewSchema.validate(req.body);
     if(error) {
         let errMsg = error.details.map((el) => el.message).join(",");
         throw new ExpressError(400, errMsg);
--- routes/review.js
+++ routes/review.js
@@ -7,17 +7,6 @@ const Review = require("../models/review.js");
 const Listing = require("../models/listing.js");
 
 
-const validateReview = (req, res, next) => {
-    let { error } = reviewSchema.validate(req.body);
-    if(error) {
-        let errMsg = error.details.map((el) => el.message).join(",");
-        throw new ExpressError(400, errMsg);
-    }
-    else {
-        next();
-    }
-} 
-
 //Post Review Route
 router.post("/", 
     validateReview , 
