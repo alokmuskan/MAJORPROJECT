5536c15bf908aeeff0df912df23276746d95a8a3
alokraj <alokraj1319@gmail.com>
2025-09-13 01:28:01 +0530

    now all reviews are associated with one author

--- routes/review.js
+++ routes/review.js
@@ -4,23 +4,22 @@ const wrapAsync = require("../utils/wrapAsync.js");
 const ExpressError = require("../utils/ExpressError.js"); 
 const Review = require("../models/review.js");
 const Listing = require("../models/listing.js");
-const { validateReview }  = require("../middleware.js");
+const { validateReview, isLoggedIn }  = require("../middleware.js");
 
 
 //Post Review Route
 router.post("/", 
+    isLoggedIn,
     validateReview , 
     wrapAsync(async(req, res) => {
     let listing = await Listing.findById(req.params.id);
     let newReview = new Review(req.body.review);
-
+    newReview.author = req.user._id;
     listing.reviews.push(newReview);
 
     await newReview.save();
     await listing.save();
 
-    // console.log("new review saved");
-    // res.send("new review saved");
     req.flash("success", "New Review Created !");
     res.redirect(`/listings/${listing._id}`);
 }));
