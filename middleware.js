47db177b4f98c0b45ba957b62e04cccda1450e1b
alokraj <alokraj1319@gmail.com>
2025-09-13 00:35:33 +0530

    changed the error msg and also the code got stuck as we were not calling the next() inside the middleware

--- middleware.js
+++ middleware.js
@@ -23,7 +23,8 @@ module.exports.isOwner = async(req, res, next) => {
     let { id } = req.params;
     let listing = await Listing.findById(id);
     if(!listing.owner.equals(res.locals.currUser._id)) {
-        req.flash("error", "You dont have permission to edit");
+        req.flash("error", "You are not the owner of this listing. ");
         return res.redirect(`/listings/${id}`);
     }
-}
+    next();
+};
