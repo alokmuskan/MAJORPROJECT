8cd8f293745710c54c25ce36fb2f5f7f10c745cb
alokraj <alokraj1319@gmail.com>
2025-09-13 00:53:24 +0530

    everything works well till now

--- routes/review.js
+++ routes/review.js
@@ -2,9 +2,9 @@ const express = require("express");
 const router = express.Router({mergeParams: true});
 const wrapAsync = require("../utils/wrapAsync.js");
 const ExpressError = require("../utils/ExpressError.js"); 
-const { reviewSchema } = require("../schema.js");
 const Review = require("../models/review.js");
 const Listing = require("../models/listing.js");
+const { validateReview }  = require("../middleware.js");
 
 
 //Post Review Route
