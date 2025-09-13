9347a301cb4522400c15595c31fc20c6311c023c
alokraj <alokraj1319@gmail.com>
2025-09-13 23:17:06 +0530

    required controllers and passed async index function thru index route

--- controllers/listings.js
+++ controllers/listings.js
@@ -1,3 +1,5 @@
+const Listings = require("../models/listings");
+
 module.exports.index = async(req, res) => {
     const allListings = await Listing.find({});
     res.render("listings/index.ejs", { allListings });
