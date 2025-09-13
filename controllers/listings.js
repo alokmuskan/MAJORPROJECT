ec79b85cf3482921a8dbc158a52ed69978f8019f
alokraj <alokraj1319@gmail.com>
2025-09-13 23:28:58 +0530

    there were some error while requiring listing models in controllers

--- controllers/listings.js
+++ controllers/listings.js
@@ -1,4 +1,4 @@
-const Listings = require("../models/listing");
+const Listing = require("../models/listing");
 
 module.exports.index = async(req, res) => {
     const allListings = await Listing.find({});
