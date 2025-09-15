8dc45d5461618e01b0a2ef5b2974152c768a9f3f
alokraj <alokraj1319@gmail.com>
2025-09-16 00:21:28 +0530

    installed and required multer in listing.js inside routes so that we can parse multipart/formdata

--- routes/listing.js
+++ routes/listing.js
@@ -7,6 +7,9 @@ const { isLoggedIn, isOwner, validateListing } = require("../middleware.js");
 
 const listingController = require("../controllers/listings.js");
 
+const multer  = require('multer')
+const upload = multer({ dest: 'uploads/' })
+
 router
     .route("/")
     .get(wrapAsync(listingController.index))
