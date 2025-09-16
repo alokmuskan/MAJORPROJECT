a2de31b0ead1664c4543362a10b6e5653a0b7386
alokraj <alokraj1319@gmail.com>
2025-09-17 00:17:42 +0530

    if file doesnt exists inside request then we will get undefined in that case

--- controllers/listings.js
+++ controllers/listings.js
@@ -54,7 +54,7 @@ module.exports.updateListing = async(req, res) => {
     let { id } = req.params;
     let listing = await Listing.findByIdAndUpdate(id, {...req.body.listing});
 
-    if(typeof req.file !== "undefined") {
+    if(typeof req.file !== "undefined") { 
     let url = req.file.path;
     let filename = req.file.filename; 
     listing.image = {url, filename};
