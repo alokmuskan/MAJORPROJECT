beb4c5712e9f9bf55f9be1ed301634c2967c5d72
alokraj <alokraj1319@gmail.com>
2025-09-17 00:57:16 +0530

    used cloudinary image transformation link to reduce the previewed image quality so that need not to render high quality images every time

--- controllers/listings.js
+++ controllers/listings.js
@@ -47,7 +47,9 @@ module.exports.renderEditForm = async(req, res) => {
         req.flash("error", "Listing you requested for does not exist!");
         return res.redirect("/listings");
     }
-    res.render("listings/edit.ejs", { listing }); 
+    let originalImageUrl = listing.image.url;
+    originalImageUrl = originalImageUrl.replace("/upload", "/upload/h_300,w_250");
+    res.render("listings/edit.ejs", { listing, originalImageUrl }); 
 };
 
 module.exports.updateListing = async(req, res) => {
