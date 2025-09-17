b62d51930c9cbace2c0c9b193e8765e271117c8e
alokraj <alokraj1319@gmail.com>
2025-09-17 18:19:06 +0530

    leaving to add the functionality of forwardGeocoding here for now will do this later on

--- controllers/listings.js
+++ controllers/listings.js
@@ -28,7 +28,7 @@ module.exports.showListing = async (req, res) => {
 };
 
 module.exports.createListing = async (req, res, next) => {
-    let url = req.file.path;
+    let url = req.file.path; 
     let filename = req.file.filename; 
 
     const newListing = new Listing(req.body.listing);
